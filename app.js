c65137f7dd9d81b91104970265c84faac4e22cf9
srafailov1 <sasson.rafailov@gmail.com>
2026-05-20 18:07:49 -0400

    Increase trackpad zoom sensitivity

--- app.js
+++ app.js
@@ -222,8 +222,8 @@ const map = new mapboxgl.Map({
 });
 
 map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), "bottom-right");
-map.scrollZoom.setWheelZoomRate?.(1 / 320);
-map.scrollZoom.setZoomRate?.(1 / 240);
+map.scrollZoom.setWheelZoomRate?.(1 / 280);
+map.scrollZoom.setZoomRate?.(1 / 120);
 window.addEventListener("resize", () => map.resize());
 
 const dataStatus = document.querySelector("#dataStatus");
